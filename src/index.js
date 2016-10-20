ba3f85a856f5018a2abce53d6aa44e1db29ff982
David Garcia <david.garcia@scmspain.com>
2016-10-20 16:10:29 +0200

    renders your first ReactJS Helloworld message

--- src/index.js
+++ src/index.js
@@ -1,166 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import cx from 'classnames';
 
-const TodoMaker = (props) => {
-  const check = e => {
-    if(e.key == 'Enter'){
-      props.onAddTodo({text: e.target.value, done: false});
-      e.target.value = '';
-    }
-  }
-
+const Helloworld = (props) => {
   return (
-    <header className="header">
-      <input
-        onKeyPress={check}
-        className="new-todo"
-        type="text"
-        placeholder={props.text}/>
-    </header>
+    <section className='todoapp'>
+      <header className="header">
+        <h1>{props.title}</h1>
+      </header>
+    </section>
   );
-}
-
-class Todo extends React.Component {
-  constructor(props){
-    super(props);
-    this.handleChange = this.handleChange.bind(this)
-    this.handleClick = this.handleClick.bind(this)
-  };
-
-  handleClick(){
-    this.props.onRemoveTodo(this.props.id);
-  }
-
-  handleChange(){
-    this.props.onDone(this.props.text);
-  }
-
-  render(){
-    const classStatus = cx('input', {
-      'completed': this.props.done
-    })
-
-    return (
-      <div className="view">
-        <input id={this.props.id} onChange={this.handleChange} checked={this.props.done} className="toggle" type="checkbox" />
-        <label className={classStatus}>{this.props.text}</label>
-        <button onClick={this.handleClick} className="destroy"></button>
-      </div>
-    );
-  }
-}
-
-class TodoList extends React.Component {
-  constructor(){
-    super();
-  }
-
-  render(){
-
-    const todos = this.props.todos.map((todo, index) => {
-      return (
-        <li key={index}>
-          <Todo {...this.props} id={index} done={todo.done} text={todo.text} />
-        </li>
-      );
-    });
-
-
-    return (
-      <section className="main">
-        <ul className="todo-list">
-          {todos}
-        </ul>
-      </section>
-    );
-
-  }
-}
-
-const TodoFooter = (props) => {
-  return (
-    <footer className="footer">
-      <span className="todo-count">{props.itemsLeft} items left</span>
-    </footer>
-  );
-}
-
-class TodoApp extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      todos : []
-    }
-  }
-
-  static propTypes() {
-    return (
-      text: React.PropTypes.string
-    )
-  }
-
-  handleChange(text){
-    const newState = this.state.todos.map((todo) => {
-        if(todo.text == text){
-          return {...todo, done: !todo.done}
-        }
-
-        return todo
-    });
-
-    this.updateState({
-      todos: newState
-    })
-
-  }
-
-  updateState(newState){
-    localStorage.setItem('MyTodoAppState', JSON.stringify(newState));
-    this.setState(newState)
-  }
-
-  addTodo(todo){
-    var currentTodos = this.state.todos;
-    currentTodos.push(todo);
-    var newState = {todos: currentTodos}
-
-    this.updateState(newState);
-  }
-
-  removeTodo(todoId){
-    var currentTodos = this.state.todos;
-    currentTodos.splice(todoId, 1);
-    var newState = {todos: currentTodos}
-
-    this.updateState(newState);
-  }
-
-
-  componentDidMount(){
-    let initialState = JSON.parse(localStorage.getItem('MyTodoAppState'));
-    if (!initialState) {
-      initialState = {};
-    };
-    this.setState(initialState);
-  }
-
-  render(){
-    const itemsLeft = this.state.todos.filter((todo) => {
-      return !todo.done === true;
-    }).length;
-
-    return (
-      <div className="todoapp">
-        <TodoMaker text={this.props.text} onAddTodo={this.addTodo.bind(this)}  />
-        <TodoList todos={this.state.todos} onDone={this.handleChange.bind(this)} onRemoveTodo={this.removeTodo.bind(this)} />
-        <TodoFooter itemsLeft={itemsLeft}/>
-      </div>
-    );
-  }
 }
 
 ReactDOM.render(
-  <TodoApp text={"What\'s need to be done"}/>,
+  <Helloworld title='Hello World from props!'/>,
   document.getElementById('app')
 );
