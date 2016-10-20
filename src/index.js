6e98218a51d4343ea13d39ebb61d9a42e8c3d8db
David Garcia <david.garcia@scmspain.com>
2016-10-20 18:12:27 +0200

    console.logging strings when Enter key is pressed

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const TodoMaker = (props) => {
+  const logger = (e) => {
+    console.log(e.target.value)
+    e.target.value = ''
+  }
+
+  const onKeyPress = (e) => {
+    e.key === 'Enter' ? logger(e) : null
+  }
+
   return (
     <section className='todoapp'>
       <header className="header">
         <h1>{props.title}</h1>
-        <input className='new-todo' placeholder={props.message} type={'text'}/>
+        <input className='new-todo' onKeyPress={onKeyPress} placeholder={props.message} type={'text'}/>
       </header>
     </section>
   );
